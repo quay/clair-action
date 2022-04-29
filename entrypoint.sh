d6a64d33c483a915cb20449a27240095288e05e4
crozzy <joseph.crosland@gmail.com>
2022-04-29 09:29:55 -0700

    docker: more arg parsing
    Signed-off-by: crozzy <joseph.crosland@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 set -e
 
-local-clair report --db-path=/matcher ${@:2} > $1
+while getopts "r:p:f:o:c:" o; do
+   case "${o}" in
+       r)
+         export imageRef="$(sed -e 's/^[ \t]*//'<<<"${OPTARG}")"
+       ;;
+       p)
+         export imagePath="$(sed -e 's/^[ \t]*//'<<<"${OPTARG}")"
+       ;;
+       f)
+         export format="$(sed -e 's/^[ \t]*//'<<<"${OPTARG}")"
+       ;;
+       o)
+         export output="$(sed -e 's/^[ \t]*//'<<<"${OPTARG}")"
+       ;;
+       c)
+         export returnCode="$(sed -e 's/^[ \t]*//'<<<"${OPTARG}")"
+       ;;
+  esac
+done
+
+local-clair report \
+    --db-path=/matcher \
+    --image-path=${GITHUB_WORKSPACE}/${imagePath} \
+    --image-ref=${imageRef} \
+    --format=${format} > ${output}
 
 exit $returnCode
