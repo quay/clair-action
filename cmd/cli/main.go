deb1f7ae03c62d3b8f4eddf9014d7b4f26ca8e12
crozzy <joseph.crosland@gmail.com>
2023-02-14 09:51:43 -0800

    logging: Straighten the logging story out a little
    Add the ability to change log level with a flag and change to a more
    CLI friendly logging format.
    
    Signed-off-by: crozzy <joseph.crosland@gmail.com>

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
+	"github.com/quay/zlog"
+	"github.com/rs/zerolog"
 	"github.com/urfave/cli/v2"
+)
+
+var (
+	logout = zerolog.New(&zerolog.ConsoleWriter{
+		Out:        os.Stderr,
+		TimeFormat: time.RFC3339,
+	}).Level(zerolog.InfoLevel).
+		With().
+		Timestamp().
+		Logger()
 )
 
 func main() {
@@ -14,9 +28,29 @@ func main() {
 		Usage:                "clair-action --help",
 		Description:          "A CLI application for running Clair v4 locally",
 		EnableBashCompletion: true,
+		Before: func(c *cli.Context) error {
+			level, err := zerolog.ParseLevel(c.String("level"))
+			if err != nil {
+				return fmt.Errorf("unknown log level %s", c.String("level"))
+			}
+			logout = logout.Level(level)
+			if c.String("level") == "" {
+				logout = logout.Level(zerolog.InfoLevel)
+			}
+			zlog.Set(&logout)
+			return nil
+		},
+
 		Commands: []*cli.Command{
 			reportCmd,
 			updateCmd,
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "level",
+				Aliases: []string{"l"},
+				Usage:   "specify level log for output (debug, warn, info, disabled, default: info)",
+			},
 		},
 	}
 
